6c9df7fb401a3c41dc6c62b85d5e36bf9c94b1b7
NII-ALLEN <odoiallen85@gmail.com>
2023-03-24 02:15:11 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,48 @@
 #include "main.h"
 
 /**
- * add - a function that adds two integers and returns the result
- * @x: the first integer to be added
- * @y: the second integer to be added
- * Return: the result of the addition
+ * print_times_table - a function that prints the n times table,
+ * starting with 0.
+ * @n: integer expected
  */
-int add(int x, int y)
+void print_times_table(int n)
 {
-	return (x + y);
+	int x, y, z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = y * x;
+				if (y == 0)
+				{
+					_putchar(z + '0');
+				} else if (z < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				} else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				} else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
